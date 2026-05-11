ee9875b8a5721b6f9ebb2de9d6037b34f43b3c90
Adeeb <malikadeeb00012@gmail.com>
2026-05-11 20:03:50 +0530

    final pixel fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
-import "./globals.css";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "BOTKET FOREX",
-  description: "Learn automated forex trading with MT4 bots",
-};
+import "./globals.css"
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
@@ -20,10 +14,17 @@ export default function RootLayout({
               !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              if(!f._fbq)f._fbq=n;
+              n.push=n;
+              n.loaded=!0;
+              n.version='2.0';
+              n.queue=[];
+              t=b.createElement(e);
+              t.async=!0;
+              t.src=v;
+              s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+              }(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
 
               fbq('init', '1009132421667353');
@@ -47,5 +48,5 @@ export default function RootLayout({
         </noscript>
       </body>
     </html>
-  );
+  )
 }
