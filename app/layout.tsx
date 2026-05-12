07421eca33d81bfe2f1945a1e8f9088c34fb6157
Adeeb <malikadeeb00012@gmail.com>
2026-05-12 10:33:15 +0530

    meta pixel connected

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,52 +1,60 @@
-import "./globals.css"
+import "./globals.css";
+import Script from "next/script";
+
+export const metadata = {
+  title: "Botket Forex",
+  description: "Learn automated forex trading with MT4 bots",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;
-              n.push=n;
-              n.loaded=!0;
-              n.version='2.0';
-              n.queue=[];
-              t=b.createElement(e);
-              t.async=!0;
-              t.src=v;
-              s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)
-              }(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
 
-              fbq('init', '1009132421667353');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1009132421667353');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        {/* End Meta Pixel Code */}
+
       </head>
 
       <body>
-        {children}
-
         <noscript>
           <img
             height="1"
             width="1"
             style={{ display: "none" }}
             src="https://www.facebook.com/tr?id=1009132421667353&ev=PageView&noscript=1"
-            alt=""
           />
         </noscript>
+
+        {children}
       </body>
     </html>
-  )
+  );
 }
