68aec9986cac72a4c926fbdc03f370cb5786c9d2
Adeeb <malikadeeb00012@gmail.com>
2026-06-23 13:11:44 +0530

    Fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import "./globals.css";
 import Script from "next/script";
 
 export default function RootLayout({
@@ -16,7 +17,7 @@ export default function RootLayout({
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)n=f.fbq;
+            if(!f._fbq)f._fbq=n;
             n.push=n;
             n.loaded=!0;
             n.version='2.0';
